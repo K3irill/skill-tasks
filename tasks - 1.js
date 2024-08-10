e79aea19d7d91dda862321bcd0ff7c8c0bf7fb55
K3rill <razdor.2019@mail.ru>
2024-08-10 22:07:47 +0300

    tasks

--- tasks - 1.js
+++ tasks - 1.js
@@ -51,24 +51,27 @@ function whatday(num) {
     "Friday",
     "Saturday",
   ];
-let result = '';
-    if (num === 1) {
-      result = ARR[0];
-      return console.log(ARR[0]);
-    } else if (num === 2) {
-      return console.log(ARR[1]);
-    } else if (num === 3) {
-      return console.log(ARR[2]);
-    } else if (num === 4) {
-      return console.log(ARR[3]);
-    } else if (num === 5) {
-      return console.log(ARR[4]);
-    } else if (num === 6) {
-      return console.log(ARR[5]);
-    } else if (num === 7) {
-      return console.log(ARR[6]);
-    } else return console.log("Wrong, please enter a number between 1 and 7");
-  }
+  return ARR[num - 1] || "Wrong, please enter a number between 1 and 7";
+}
+// let result = '';
+//     if (num === 1) {
+//       result = ARR[0];
+//       return console.log(ARR[0]);
+//     } else if (num === 2) {
+//       return console.log(ARR[1]);
+//     } else if (num === 3) {
+//       return console.log(ARR[2]);
+//     } else if (num === 4) {
+//       return console.log(ARR[3]);
+//     } else if (num === 5) {
+//       return console.log(ARR[4]);
+//     } else if (num === 6) {
+//       return console.log(ARR[5]);
+//     } else if (num === 7) {
+//       return console.log(ARR[6]);
+//     } else return console.log("Wrong, please enter a number between 1 and 7");
+//   }
 
-whatday(1)
+console.log(whatday(0));
+
 
